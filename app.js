72c4a028e4fbdd3cbcc7f7fd485561565dcf12b0
YHulko <opfr95@gmail.com>
2023-07-14 16:38:51 +0300

    working...

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ $(".slider").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
+  
